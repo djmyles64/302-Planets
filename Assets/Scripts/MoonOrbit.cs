b21793515da8a4f7c20be7e53e6b677f065104e8
Danny Myles <mylesd2@ferris.edu>
2020-02-05 01:10:42 -0500

    Finished Planet and Moon orbits

---
+++ Assets/Scripts/MoonOrbit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonOrbit : MonoBehaviour
+{
+    public Transform moonOrbitCenter;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        float angle = Time.time;
+        float mag = 2;
+
+        Vector3 pos = (moonOrbitCenter == null) ? Vector3.zero : moonOrbitCenter.position;
+
+        pos.x += Mathf.Cos(angle) * mag;
+        pos.z += Mathf.Sin(angle) * mag;
+
+
+
+        transform.position = pos;
+    }
+}
---
+++ Assets/Scripts/Orbit.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class Orbit : MonoBehaviour
+{
+    public Transform orbitCenter;
+    public LineRenderer orbitPath;
+    [Range(1, 10)] public float radius = 6;
+    [Range(4, 32)] public int resolution = 8;
+    void Start()
+    {
+        orbitPath = GetComponent<LineRenderer>();
+    }
+
+    
+    void Update()
+    {
+        Vector3 pos = FindOrbitPoint(Time.time, radius);
+
+        transform.position = pos;
+
+        UpdatePoints();
+        
+    }
+
+    private Vector3 FindOrbitPoint(float angle, float mag)
+    {
+      
+        Vector3 pos = (orbitCenter == null) ? Vector3.zero : orbitCenter.position;
+
+        pos.x = Mathf.Cos(angle) * mag;
+        pos.z = Mathf.Sin(angle) * mag;
+        return pos;
+    }
+
+    /// <summary>
+    /// Set the points in the LineRenderer
+    /// </summary>
+    void UpdatePoints()
+    {
+        
+        Vector3[] points = new Vector3[resolution];
+
+        for(int i = 0; i < points.Length; i++)
+        {
+            float p = i / (float)points.Length;
+            float radians = p * Mathf.PI * 2;
+            points[i] = FindOrbitPoint(radians, radius);
+
+
+        }
+        orbitPath.positionCount = resolution;
+        orbitPath.SetPositions(points);
+    }
+}
